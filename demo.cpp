f4a9d35ab294d5c13723e38e596964b334e0033e
Jared Hoberock <jaredhoberock@gmail.com>
2013-06-02 20:14:18 -0700

    Test things a bit more rigorously

--- demo.cpp
+++ demo.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <numeric>
 #include <cstdlib>
+#include <utility>
 #include "first_fit_malloc.hpp"
 
 
@@ -12,18 +13,58 @@ int main()
 {
   for(int i = 0; i < 100; ++i)
   {
-    size_t n = std::rand() % (1 << 10);
+    size_t num_allocations = 50;
 
-    // malloc something
-    int *ptr = static_cast<int*>(first_fit_malloc(n * sizeof(int)));
+    void **allocations = static_cast<void**>(first_fit_malloc(num_allocations * sizeof(void*)));
 
-    std::iota(ptr, ptr + n, 0);
+    for(int j = 0; j < num_allocations; ++j)
+    {
+      size_t n = std::rand() % (1 << 10);
 
-    assert(std::is_sorted(ptr, ptr + n));
+      int *ptr = static_cast<int*>(first_fit_malloc(n * sizeof(int)));
 
-    first_fit_free(ptr);
-  }
+      std::iota(ptr, ptr + n, 0);
 
-  return 0;
-}
+      assert(std::is_sorted(ptr, ptr + n));
+
+      allocations[j] = ptr;
+    }
+
+    // free some of them
+    for(int j = 0; j < num_allocations; ++j)
+    {
+      if((std::rand() % 2) == 0)
+      {
+        first_fit_free(allocations[j]);
+        allocations[j] = 0;
+      } // end if
+    } // end for j
+
+    // make some new allocations
+    for(int j = 0; j < num_allocations; ++j)
+    {
+      if((std::rand() % 2) == 0 && allocations[j] == 0)
+      {
+        size_t n = std::rand() % (1 << 10);
+
+        int *ptr = static_cast<int*>(first_fit_malloc(n * sizeof(int)));
+
+        std::iota(ptr, ptr + n, 0);
+
+        assert(std::is_sorted(ptr, ptr + n));
+
+        allocations[j] = ptr;
+      } // end if
+    } // end for j
+
+    // now free everything
+    for(int j = 0; j < num_allocations; ++j)
+    {
+      first_fit_free(allocations[j]);
+    } // end for j
+
+    first_fit_free(allocations);
+  } // end for i
+} // end main
+
 
