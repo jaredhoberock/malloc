6fcf4c711664372cf1e09e2f09d6f226505d368f
Jared Hoberock <jaredhoberock@gmail.com>
2013-06-02 17:00:23 -0700

    Implement free

--- first_fit_malloc.hpp
+++ first_fit_malloc.hpp
@@ -1,15 +1,18 @@
 #pragma once
 
+// inspired by http://www.inf.udec.cl/~leo/Malloc_tutorial.pdf
+
 
 struct block
 {
   size_t  size;
+  block  *prev;
   block  *next; // XXX isn't this just data(block) + size?
   int     is_free;
 };
 
 
-char *data(block *b)
+void *data(block *b)
 {
   return reinterpret_cast<char*>(b) + sizeof(block);
 }
@@ -20,11 +23,12 @@ void split_block(block *b, size_t size)
   block *new_block;
 
   // emplace a new block within the old one's data segment
-  new_block = reinterpret_cast<block*>(data(b) + size);
+  new_block = reinterpret_cast<block*>(reinterpret_cast<char*>(data(b)) + size);
 
   // the new block's size is the old block's size less the size of the split less the size of a block
   new_block->size = b->size - size - sizeof(block);
 
+  new_block->prev = b;
   new_block->next = b->next;
   new_block->is_free = true;
 
@@ -33,6 +37,36 @@ void split_block(block *b, size_t size)
 
   // link the old block to the new one
   b->next = new_block;
+  if(new_block->next)
+  {
+    new_block->next->prev = new_block;
+  } // end if
+} // end split_block()
+
+
+bool fuse_block(block *b)
+{
+  if(b->next && b->next->is_free)
+  {
+    b->size += sizeof(block) + b->next->size;
+    b->next = b->next->next;
+
+    if(b->next)
+    {
+      b->next->prev = b;
+    }
+
+    return true;
+  }
+
+  return false;
+}
+
+
+block *get_block(void *data)
+{
+  // the block metadata lives sizeof(block) bytes to the left of data
+  return reinterpret_cast<block *>(reinterpret_cast<char *>(data) - sizeof(block));
 }
 
 
@@ -69,6 +103,7 @@ block *extend_heap(block *prev, size_t size)
   }
 
   new_block->size = size;
+  new_block->prev = prev;
   new_block->next = 0;
   new_block->is_free = false;
 
@@ -138,6 +173,43 @@ void *first_fit_malloc(size_t size)
 
 void first_fit_free(void *ptr)
 {
-  // just leak it
-}
+  if(ptr != 0)
+  {
+    block *b = get_block(ptr);
+
+    // free the block
+    b->is_free = true;
+
+    // try to fuse the freed block the previous block
+    // XXX we could instead attempt to fuse with the next block if it is free
+    //     this way maybe we could do without the prev pointer
+    if(b->prev && b->prev->is_free)
+    {
+      b = b->prev;
+      fuse_block(b);
+    } // end if
+
+    // now try to fuse with the next block
+    if(b->next)
+    {
+      fuse_block(b);
+    } // end if
+    else
+    {
+      // we're at the end of the heap
+      if(b->prev)
+      {
+        b->prev->next = 0;
+      } // end if
+      else
+      {
+        // we freed the last block
+        base = 0;
+      } // end else
+
+      // the the OS know where the new break is
+      brk(b);
+    } // end else
+  } // end if
+} // end first_fit_free()
 
